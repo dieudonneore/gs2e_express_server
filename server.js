ded210e6f63013cfc13b023e84450dd060aabeed
dore <dore772@gmail.com>
2022-07-05 14:21:38 +0000

    express js server to retrieve data from request user with validator

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express'); // import express to create a new server
+const bodyParser = require('body-parser');// import body-parser 
+
+
+const app = express();// instanciate express in app variable
+
+app.use(express.json());// let app use express.json to retrieve data in json format
+
+const port="2020";// create a port
+
+const { body, validationResult } = require('express-validator');// this is to validate all entries specify
+
+
+// validator function to controll users input
+const validatorWithexpress = () => {
+    return [
+        body('username').isEmail(),
+        body('password').isLength({min:7}),
+        body('description').isLength({min:2})
+    ];
+}
+
+// get request to see if our server is working
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+// post request to retrieve all the entries made by user
+app.post(
+    '/gs2e/data',// endpoints
+    validatorWithexpress()// validator function
+    ,
+    (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            res.json({
+                "message": req.body
+            })
+        }
+    )
+
+app.listen(port)
