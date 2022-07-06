6ca82336fc2d69cbdee85fcdec3ed3db6669244e
dore <dore772@gmail.com>
2022-07-06 14:09:40 +0000

    update server.js, using router, and validator field module

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express'); // import express to create a new server
-const bodyParser = require('body-parser');// import body-parser 
+const bodyParser = require('body-parser');// import body-parser
 
+const tasks = require('./routes/tasksRouter');
+const expressValidator = require('./validators/expressValidator');
+const fieldValidator = require('./validators/fieldValidator');
+const port="2020";// create a port
 
 const app = express();// instanciate express in app variable
 
 app.use(express.json());// let app use express.json to retrieve data in json format
 
-const port="2020";// create a port
 
 
 
-const tasks = require('./routes/tasksRouter');
-const expressValidator = require('./validators/expressValidator');
-const fieldValidator = require('./validators/fieldValidator');
-const { body, validationResult } = require('express-validator');
 
 app.use('/tasks', tasks);
 
